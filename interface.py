1924a7fe0192137c6cbf23c7acec2350ce8a911e
Rafael-Bruno314 <rafaelbruno.cunha@gmail.com>
2024-06-29 12:34:32 -0300

    Início

---
+++ interface.py
@@ -0,0 +1,8 @@
+print("Bem vindo ao gerenciador de bibliotecas!")
+
+escolha_do_usuario = print("O que você deseja fazer?")
+print("1 - Adicionar novo livro")
+print("2 - Remover livro existente")
+print("3 - Atualizar informações de um livro")
+print("4 - Listar todos os livros disponíveis")
+print("-"*60)
